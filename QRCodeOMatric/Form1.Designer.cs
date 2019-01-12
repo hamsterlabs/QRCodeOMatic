1da7833192604412a97ad22979191acc6678f31e
dmarti19 <dmarti19@utk.edu>
2019-01-11 23:18:39 -0500

    Initial check in

---
+++ QRCodeOMatric/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace HamsterLabs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbox = new System.Windows.Forms.PictureBox();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.txtEncode = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtDecode = new System.Windows.Forms.TextBox();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAbout = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbox
+            // 
+            this.pbox.BackColor = System.Drawing.SystemColors.Window;
+            this.pbox.Location = new System.Drawing.Point(16, 16);
+            this.pbox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pbox.Name = "pbox";
+            this.pbox.Size = new System.Drawing.Size(373, 345);
+            this.pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbox.TabIndex = 0;
+            this.pbox.TabStop = false;
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.Location = new System.Drawing.Point(821, 101);
+            this.btnEncode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(100, 28);
+            this.btnEncode.TabIndex = 1;
+            this.btnEncode.Text = "Encode";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // txtEncode
+            // 
+            this.txtEncode.Location = new System.Drawing.Point(457, 53);
+            this.txtEncode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtEncode.Name = "txtEncode";
+            this.txtEncode.Size = new System.Drawing.Size(463, 22);
+            this.txtEncode.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(289, 382);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 28);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save...";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(17, 382);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(100, 28);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "Open...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtDecode
+            // 
+            this.txtDecode.Location = new System.Drawing.Point(457, 191);
+            this.txtDecode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtDecode.Name = "txtDecode";
+            this.txtDecode.Size = new System.Drawing.Size(463, 22);
+            this.txtDecode.TabIndex = 5;
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.Location = new System.Drawing.Point(821, 242);
+            this.btnDecode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(100, 28);
+            this.btnDecode.TabIndex = 6;
+            this.btnDecode.Text = "Decode";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(847, 449);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 28);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 481);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "MessagingToolkit.QRCode.DLL  Example";
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(872, 12);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(75, 23);
+            this.btnAbout.TabIndex = 9;
+            this.btnAbout.Text = "&About...";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 517);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.txtDecode);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtEncode);
+            this.Controls.Add(this.btnEncode);
+            this.Controls.Add(this.pbox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "QR Code ";
+            ((System.ComponentModel.ISupportInitialize)(this.pbox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbox;
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.TextBox txtEncode;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtDecode;
+        private System.Windows.Forms.Button btnDecode;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAbout;
+    }
+}
+
---
+++ QRCodeOMatric/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+
+namespace HamsterLabs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncode_Click(object sender, EventArgs e)
+        {
+            string str = txtEncode.Text;
+            if (String.IsNullOrWhiteSpace(str)) return;
+            QRCodeEncoder enc = new QRCodeEncoder();
+            Bitmap qrcode = enc.Encode(str);
+            pbox.Image = qrcode as Image;
+
+            txtDecode.Text = string.Empty;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Image Files (.jpg|*.jpg|.png|*.png|All Files (*.*)|*.*)";
+            if (DialogResult.OK == dlg.ShowDialog(this))
+            {
+                try
+                {
+                    Bitmap qr = (Bitmap)pbox.Image;
+                    string file = dlg.FileName;
+                    qr.Save(file);
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show(x.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            txtEncode.Text = string.Empty;
+            txtDecode.Text = string.Empty;
+
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Image Files (.jpg|*.jpg|.png|*.png|All Files (*.*)|*.*)";
+            dlg.CheckPathExists = true;
+            dlg.Title = "Open Image File";
+            dlg.Multiselect = false;
+            if (DialogResult.OK == dlg.ShowDialog(this))
+            {
+
+                try
+                {
+                    //Bitmap img = (Bitmap)Image.FromFile(dlg.FileName);
+                    pbox.Image = Image.FromFile(dlg.FileName);
+
+                    QRCodeDecoder dec = new QRCodeDecoder();
+                    txtDecode.Text = (dec.Decode(new QRCodeBitmapImage(pbox.Image as Bitmap)));
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show(x.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnDecode_Click(object sender, EventArgs e)
+        {
+            QRCodeDecoder dec = new QRCodeDecoder();
+            txtDecode.Text = (dec.Decode(new QRCodeBitmapImage(pbox.Image as Bitmap)));       
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtDecode.Text = String.Empty;
+            txtEncode.Text = String.Empty;
+            pbox.Image = null;
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "QRCodeOMatic 0.1", "HamsterLabs", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
